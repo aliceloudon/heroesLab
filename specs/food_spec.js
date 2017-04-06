0b82f81d3f33348abacdea23884ab4e7790336d0
Alice Loudon <alice_loudon@icloud.com>
2017-04-06 14:59:44 +0100

    Task has urgency level, reward and completion status. Pass tests

---
+++ specs/food_spec.js
@@ -0,0 +1,18 @@
+var assert = require("assert");
+var Food = require("../food.js");
+ 
+describe("food" , function(){
+
+  var food;
+  var food2;
+
+  beforeEach(function(){
+    food = new Food( "doughnuts" , 500 );
+    food2 = new Food( "kryptonite" , -1500 );
+  })
+
+  it('should have a name', function(){
+    assert.strictEqual("doughnuts", food.name)
+  })
+
+}) 
